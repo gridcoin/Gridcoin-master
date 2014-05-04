7eb5008c52ab8a47a0e3515a8b0a8273f00f0cfc
gridcoin <contact@gridcoin.us>
2014-05-04 16:23:04 -0500

    CPUMiner18

--- build/moc_bitcoingui.cpp
+++ build/moc_bitcoingui.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'bitcoingui.h'
 **
-** Created: Sun May 4 13:33:10 2014
+** Created: Sun May 4 16:21:04 2014
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.4)
 **
 ** WARNING! All changes made in this file will be lost!
